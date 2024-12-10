77bccddd869ce53931db49b7660fb8bec32504a9
ajripa <ajripa@ie.edu>
2024-12-10 17:39:32 +0100

    more fixes in create-operator-user

--- create-operator-user/scripts/call_oracle.js
+++ create-operator-user/scripts/call_oracle.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const api_url = "http://api_connector:3010"
+const api_url = process.env.API_CONNECTOR_URL || "http://api_connector:3010"
 const route = "oracle"
 const ADMIN_TOKEN = process.env.ADMIN_TOKEN
 const VERAMO_API_CRED = process.env.VERAMO_API_CRED
